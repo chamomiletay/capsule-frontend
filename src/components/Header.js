9321b3fcfc7fefdfb6746be361c4fd400d229f51
chamomiletay <chamomile.tay@gmail.com>
2022-07-12 22:54:25 -0400

    Additional Header styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,8 @@
 //----- Imports -----//
 import React from 'react'
 import { Link } from 'react-router-dom'
-// import ReactDOM from 'react-dom'
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import { faShirt } from '@fortawesome/free-solid-svg-icons'
 import './Header.css';
 
-// const element = <FontAwesomeIcon icon={faShirt} />
-
-// ReactDOM.render(element, document.body)
 
 const Header = () => {
   return (
@@ -38,7 +32,7 @@ const Header = () => {
               <Link to={''}>
                 <li>new outfit</li>
               </Link>
-              
+
             </ul>
 
         </div>
